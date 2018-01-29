9a48a00093355cc71b90dfea51dd7fbd7e7173ed
Adam Kupczyk <akupczyk@redhat.com>
2018-01-29 10:15:10 +0100

    Added missing call_start.asm
    Signed-off-by: Adam Kupczyk <akupczyk@redhat.com>

---
+++ call_start.asm
@@ -0,0 +1,33 @@
+.globl call_start
+.data
+L0:
+	.asciz "A=1"
+L1:
+	.asciz "program"
+
+.text
+.align 16
+
+
+call_start:
+	//arg3 - rdx, auxv
+	//arg4 - rcx, auxv_size
+	sub %rcx, %rsp
+	add %rcx, %rdx
+1:
+	sub $8, %rdx
+	mov (%rdx), %rax
+	pushq %rax
+	sub $8, %rcx
+	jnz 1b
+
+	mov %rdi, %rax
+	mov %rsi, %rdx
+
+	pushq $0
+	pushq $L0
+	pushq $0
+	pushq $L1
+	pushq $1
+	jmp *%rax
+
